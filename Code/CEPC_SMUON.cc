dd7c2a5c44ed8d89d6a955cc307eb1267546b07e
BudinG <zhupengxuan326@hotmail.com>
2022-09-10 06:06:22 +0800

    Prepare for paper

--- Code/CEPC_SMUON.cc
+++ Code/CEPC_SMUON.cc
@@ -10,12 +10,7 @@
 #include "Rivet/Projections/MissingMomentum.hh"
 #include "Rivet/Tools/Cutflow.hh"
 #include <tuple>
-<<<<<<< HEAD
 
-namespace Rivet {
-=======
-#include <math.h>
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
 
 namespace Rivet
 {
@@ -33,7 +28,7 @@ namespace Rivet
     void init()
     {
 
-// Initialise and register projections
+      // Initialise and register projections
 
       // The basic final-state projection:
       // all final-state particles within
@@ -43,11 +38,7 @@ namespace Rivet
       const FinalState fs(Cuts::abseta < 3.0);
       declare(VisibleFinalState(Cuts::abseta < 3.0), "vfs");
 
-<<<<<<< HEAD
-      Cut lepton_cuts = ((Cuts::abseta < 3.0) && (Cuts::pT > 5 * GeV));
-=======
       Cut lepton_cuts = ((Cuts::abseta < 3.0) && (Cuts::E > 0.5 * GeV));
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
 
       // FinalState bare_elec(Cuts::abspid == PID::ELECTRON);
       // FinalState bare_muon(Cuts::abspid == PID::MUON);
@@ -66,13 +57,10 @@ namespace Rivet
 
       // Dress the prompt bare leptons with prompt photons within dR < 0.1,
       // and apply some fiducial cuts on the dressed leptons
-<<<<<<< HEAD
       // DressedLeptons elec_drs(photons, bare_elec, 0.1, lepton_cuts);
       // DressedLeptons muon_drs(photons, bare_muon, 0.1, lepton_cuts);
-=======
       DressedLeptons elec_drs(photons, bare_elec, 0.1, lepton_cuts);
       DressedLeptons muon_drs(photons, bare_muon, 0.1, lepton_cuts);
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
       // declare(muon_drs, "muons");
       // declare(elec_drs, "elecs");
       declare(bare_elec, "elecs");
@@ -83,43 +71,12 @@ namespace Rivet
 
       // Book histograms
       // specify custom binning
-<<<<<<< HEAD
-
       // take binning from reference data using HEPData ID (digits in "d01-x01-y01" etc.)
-
-      book(_hist_mT2, "hist_mT2", 25, 0., 125.);
-      book(_hist_mll, "hist_mll", 40, 0., 200.);
-      book(_hist_ETmiss, "hist_ETmiss", 20, 0.0, 100.);
-      book(_hist_cThetalp, "hist_CosThetalp", 20, -1., 1.);
-      book(_hist_cThetalm, "hist_CosThetalm", 20, -1., 1.);
-
-      book(_NH_mT2, "normlizedhist_mT2", 25, 0., 125.);
-      book(_NH_mll, "normlizedhist_mll", 40, 0., 200.);
-      book(_NH_ETmiss, "normlizedhist_ETmiss", 20, 0.0, 100.);
-      book(_NH_cThetalp, "normlizedhist_CosThetalp", 20, -1., 1.);
-      book(_NH_cThetalm, "normlizedhist_CosThetalm", 20, -1., 1.);
-
-      // Book Cut-flows
-      const strings cfnames = {
-          "No-Cut",
-          "Selection",
-          "MT2",
-          "cosTheta"};
-
-      _Cutflows_ee.addCutflow("ee", cfnames);
-      _Cutflows_em.addCutflow("em", cfnames);
-      _Cutflows_mm.addCutflow("mm", cfnames);
-
-      // Book SR counters
-      // for (size_t i = 0; i < 3; ++i)
-      // {
-      //   book(_srcounts[i], "SR_" + toString(i));
-      // }
-=======
       book(_SR_lowDELTAM, "SR_LOWDELTAM", 5, 0, 5);
       book(_SR_midDELTAM, "SR_MIDDELTAM", 6, 0, 6);
       book(_SR_highDELTAM, "SR_HIGHDELTAM", 4, 0, 4);
 
+      // Book Cut-flows
       const strings cfnames = {
           "No Cuts",
           "SR-High DeltaM",
@@ -168,37 +125,25 @@ namespace Rivet
                                      "SR03 DeltaM < 30",
                                      "SR04 DeltaM < 40"});
 
-      book(_hist_Elm, "hist_ELm", 1500, 0., 1500.);
-      book(_hist_Elp, "hist_ELp", 1500, 0., 1500.);
+      book(_hist_Elm, "hist_ELm", 150, 0., 150.);
+      book(_hist_Elp, "hist_ELp", 150, 0., 150.);
       book(_hist_dRlmRec, "hist_dR_lm_Recoil", 100, 0., 20.);
       book(_hist_dRlpRec, "hist_dR_lp_Recoil", 100, 0., 20.);
-      book(_hist_mll, "hist_mll", 3000, 0., 3000.);
-      book(_hist_mRecoil, "hist_mRecoil", 3000, 0., 3000.);
+      book(_hist_mll, "hist_mll", 250, 0., 250.);
+      book(_hist_mRecoil, "hist_mRecoil", 250, 0., 250.);
 
-      book(_hist_mRCmin, "hist_mRCmin", 1500, 0., 1500.);
-      book(_hist_mRCmax, "hist_mRCmax", 1500, 0., 1500.);
-      book(_hist_mLSPmax, "hist_mLSPmax", 1500, 0., 1500.);
-      book(_hist_mRCdM, "hist_mRCdM", 1500, 0., 1500.);
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
+      book(_hist_mRCmin, "hist_mRCmin", 150, 0., 150.);
+      book(_hist_mRCmax, "hist_mRCmax", 150, 0., 150.);
+      book(_hist_mLSPmax, "hist_mLSPmax", 150, 0., 150.);
+      book(_hist_mRCdM, "hist_mRCdM", 150, 0., 150.);
     }
 
     /// Perform the per-event analysis
     void analyze(const Event &event)
     {
-<<<<<<< HEAD
-
-      _Cutflows_ee.fillinit();
-      _Cutflows_em.fillinit();
-      _Cutflows_mm.fillinit();
-
-      _Cutflows_ee.fill(1);
-      _Cutflows_em.fill(1);
-      _Cutflows_mm.fill(1);
-
-      const double weight = 1.0;
-      const MissingMomentum met = apply<MissingMomentum>(event, "MET");
-      double ETmiss = met.missingEt();
-=======
+      // const double weight = 1.0;
+      // const MissingMomentum met = apply<MissingMomentum>(event, "MET");
+      // double ETmiss = met.missingEt();
       // const double weight = 1.0;
       _Cutflows.fillinit();
       _Cutflows.fill(1);
@@ -211,187 +156,15 @@ namespace Rivet
 
       _CF_MidDM.fillinit();
       _CF_MidDM.fill(1);
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
 
       FourMomentum pTmiss;
       for (const Particle &p : apply<VisibleFinalState>(event, "vfs").particles())
       {
         pTmiss -= p.momentum();
       }
-<<<<<<< HEAD
       // double ETmiss = pTmiss.pT();
       // Retrieve dressed leptons, sorted by pT
       // vector<DressedLepton> leptons = apply<DressedLeptons>(event, "leptons").dressedLeptons();
-      const Particles &elecFS = apply<FinalState>(event, "elecs").particlesByPt();
-      const Particles &muonFS = apply<FinalState>(event, "muons").particlesByPt();
-      const Particles &leptFS = elecFS + muonFS;
-      sortByPt(leptFS);
-
-      bool eeMET = false;
-      bool emMET = false;
-      bool mmMET = false;
-
-      const int nelec = elecFS.size();
-      const int nmuon = muonFS.size();
-
-      const ParticlePair &beams = apply<Beam>(event, "Beams").beams();
-      Vector3 axis;
-      if (beams.first.charge() > 0)
-      {
-        axis = beams.first.momentum().p3().unit();
-      }
-      else
-      {
-        axis = beams.second.momentum().p3().unit();
-      }
-
-      if (nelec == 2 && nmuon == 0)
-      {
-        if (elecFS[0].charge() * elecFS[1].charge() < 0)
-        {
-          eeMET = true;
-        }
-      }
-      if (nelec == 0 && nmuon == 2)
-      {
-        if (muonFS[0].charge() * muonFS[1].charge() < 0)
-        {
-          mmMET = true;
-        }
-      }
-      if (nelec == 1 && nmuon == 1)
-      {
-        if (elecFS[0].charge() * muonFS[0].charge() < 0)
-        {
-          emMET = true;
-        }
-      }
-
-      if (ETmiss < 5. * GeV)
-        vetoEvent;
-
-      if (eeMET)
-      {
-        double mll = (elecFS[0].momentum() + elecFS[1].momentum()).mass();
-        if ((mll < 80. * GeV) || (mll > 100. * GeV))
-        {
-          _Cutflows_ee.fill(2);
-          const double m_T2 = mT2(elecFS[0].momentum(), elecFS[1].momentum(), pTmiss, 0.0);
-          // _hist_mT2->fill(m_T2, weight);
-
-          if (m_T2 > 50. * GeV)
-          {
-            _Cutflows_ee.fill(3);
-          }
-
-          double ctheta_lp;
-          double ctheta_lm;
-          if (elecFS[0].charge() > 0)
-          {
-            ctheta_lp = elecFS[0].momentum().p3().unit().dot(axis);
-            ctheta_lm = elecFS[1].momentum().p3().unit().dot(axis);
-          }
-          else
-          {
-            ctheta_lp = elecFS[1].momentum().p3().unit().dot(axis);
-            ctheta_lm = elecFS[0].momentum().p3().unit().dot(axis);
-          }
-          // _hist_cThetalp->fill(ctheta_lp);
-          // _hist_cThetalm->fill(ctheta_lm);
-
-          if (ctheta_lp < 0.3 && ctheta_lm > -0.3)
-          {
-            _Cutflows_ee.fill(4);
-          }
-        }
-      }
-      else if (mmMET)
-      {
-        nPass += 1.;
-
-        double mll = (muonFS[0].momentum() + muonFS[1].momentum()).mass();
-        if ((mll < 80. * GeV) || (mll > 100. * GeV))
-        {
-          // {
-
-          const double m_T2 = mT2(muonFS[0].momentum(), muonFS[1].momentum(), pTmiss, 0.0);
-          _hist_mT2->fill(m_T2, weight);
-          _NH_mT2->fill(m_T2, weight);
-
-          // else
-          //   vetoEvent;
-
-          double ctheta_lp;
-          double ctheta_lm;
-          if (muonFS[0].charge() > 0)
-          {
-            ctheta_lp = muonFS[0].momentum().p3().unit().dot(axis);
-            ctheta_lm = muonFS[1].momentum().p3().unit().dot(axis);
-          }
-          else
-          {
-            ctheta_lp = muonFS[1].momentum().p3().unit().dot(axis);
-            ctheta_lm = muonFS[0].momentum().p3().unit().dot(axis);
-          }
-          _hist_cThetalp->fill(ctheta_lp);
-          _hist_cThetalm->fill(ctheta_lm);
-          _NH_cThetalp->fill(ctheta_lp);
-          _NH_cThetalm->fill(ctheta_lm);
-          _Cutflows_mm.fill(2);
-          if (m_T2 > 20. * GeV)
-          {
-          _hist_ETmiss->fill(ETmiss, weight);
-          _NH_ETmiss->fill(ETmiss, weight);
-          _hist_mll->fill(mll, weight);
-          _NH_mll->fill(mll, weight);
-            _Cutflows_mm.fill(3);
-            if (ctheta_lp < 0.3 && ctheta_lm > -0.3)
-            {
-              _Cutflows_mm.fill(4);
-            }
-          }
-        }
-        // }
-      }
-      else if (emMET)
-      {
-        double mll = (elecFS[0].momentum() + muonFS[0].momentum()).mass();
-        // _hist_mll->fill(mll, weight);
-        _Cutflows_em.fill(2);
-        const double m_T2 = mT2(elecFS[0].momentum(), muonFS[0].momentum(), pTmiss, 0.0);
-        // _hist_mT2->fill(m_T2, weight);
-
-        if (m_T2 > 20. * GeV)
-        {
-          _Cutflows_em.fill(3);
-        }
-
-        double ctheta_lp;
-        double ctheta_lm;
-        if (elecFS[0].charge() > 0 && muonFS[0].charge() < 0)
-        {
-          ctheta_lp = elecFS[0].momentum().p3().unit().dot(axis);
-          ctheta_lm = muonFS[0].momentum().p3().unit().dot(axis);
-        }
-        else if (elecFS[0].charge() < 0 && muonFS[0].charge() > 0)
-        {
-          ctheta_lp = muonFS[0].momentum().p3().unit().dot(axis);
-          ctheta_lm = elecFS[0].momentum().p3().unit().dot(axis);
-        }
-        else
-          vetoEvent;
-
-        // _hist_cThetalp->fill(ctheta_lp);
-        // _hist_cThetalm->fill(ctheta_lm);
-
-        if (ctheta_lp < 0.3 && ctheta_lm > -0.3)
-        {
-          _Cutflows_em.fill(4);
-        }
-      }
-      else
-        vetoEvent;
-=======
 
       const Particles &muonFS = apply<FinalState>(event, "muons").particlesByPt();
       const int nmuon = muonFS.size();
@@ -648,87 +421,36 @@ namespace Rivet
       }
       else
         return;
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
     }
 
     /// Normalise histograms etc., after the run
     void finalize()
     {
-<<<<<<< HEAD
 
       const double sf = crossSection() / (numEvents() * femtobarn);
       const double Lint = 5.e3;
       double norm = sf * Lint;
 
       MSG_INFO("Norm is " << norm);
-
-      normalize(_hist_mT2, norm);
-      normalize(_hist_mll, norm);
-      normalize(_hist_ETmiss, norm);
-      normalize(_hist_cThetalp, norm);
-      normalize(_hist_cThetalm, norm);
-
-      normalize(_NH_mT2);
-      normalize(_NH_mll);
-      normalize(_NH_ETmiss);
-      normalize(_NH_cThetalp);
-      normalize(_NH_cThetalm);
-
       MSG_INFO("Total Cross section is " << crossSection() / femtobarn << " femtobarn!");
-      _Cutflows_ee.scale(sf);
-      _Cutflows_em.scale(sf);
-      _Cutflows_mm.scale(sf);
-      // MSG_INFO("CUTFLOWS ee+ETmiss Case:\n\n"
-      //          << _Cutflows_ee);
-      // MSG_INFO("CUTFLOWS em+ETmiss Case:\n\n"
-      //          << _Cutflows_em);
-      MSG_INFO("CUTFLOWS mm+ETmiss Case:\n\n"
-               << _Cutflows_mm);
-
       // normalize(_h["XXXX"]);                                 // normalize to unity
       // normalize(_h["YYYY"], crossSection() / picobarn);      // normalize to generated cross-section in pb (no cuts)
       // scale(_h["ZZZZ"], crossSection() / picobarn / sumW()); // norm to generated cross-section in pb (after cuts)
-    }
 
-=======
-      // normalize(_h["XXXX"]); // normalize to unity
-      // normalize(_h["YYYY"], crossSection()/picobarn); // normalize to generated cross-section in pb (no cuts)
 
-      const double sf = crossSection() / (numEvents() * femtobarn);
-      const double XSect = crossSection() / femtobarn;
-      const double Luminosity = 5050.0;
-      const double norm = Luminosity * XSect;
-
-      scale(_SR_lowDELTAM, XSect / sumW()); // norm to generated cross-section in pb (after cuts)
-      scale(_SR_midDELTAM, XSect / sumW());
-      scale(_SR_highDELTAM, XSect / sumW());
-
-      scale(_hist_Elm, norm / sumW());
-      scale(_hist_Elp, norm / sumW());
-      scale(_hist_dRlmRec, norm / sumW());
-      scale(_hist_dRlpRec, norm / sumW());
-      scale(_hist_mll, norm / sumW());
-      scale(_hist_mRecoil, norm / sumW());
-      scale(_hist_mRCmin, norm / sumW());
-      scale(_hist_mRCmax, norm / sumW());
-      scale(_hist_mLSPmax, norm / sumW());
-      scale(_hist_mRCdM, norm / sumW());
-
-      _Cutflows.scale(sf * Luminosity);
+      _Cutflows.scale(sf * Lint);
       // MSG_INFO("CUTFLOWS Smuon + ETmiss Case:\n\n"
       //          << _Cutflows);
 
-      _CF_HighDM.scale(sf * Luminosity);
+      _CF_HighDM.scale(sf * Lint);
       // MSG_INFO("CUTFLOWS High DeltaM Case:\n\n"
       //          << _CF_HighDM);
 
-      _CF_LowDM.scale(sf * Luminosity);
+      _CF_LowDM.scale(sf * Lint);
       // MSG_INFO("CUTFLOWS Low DeltaM Case:\n\n"
       //          << _CF_LowDM);
 
-      _CF_MidDM.scale(sf * Luminosity);
-      MSG_INFO("CUTFLOWS Mid DeltaM Case:\n\n"
-               << _CF_MidDM);
+      _CF_MidDM.scale(sf * Lint);
     }
 
     vector<double> mRC(const FourMomentum &met, const FourMomentum &l1, const FourMomentum &l2, const FourMomentum &ISR) const
@@ -792,15 +514,9 @@ namespace Rivet
       return pos;
     }
 
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
     ///@}
 
     /// @name Histograms
-    ///@{
-    map<string, Histo1DPtr> _h;
-    map<string, Profile1DPtr> _p;
-    map<string, CounterPtr> _c;
-<<<<<<< HEAD
 
     double nPass = 0.;
     ///@}
@@ -811,22 +527,8 @@ namespace Rivet
     Cutflows _Cutflows_mm;
     Cutflows _Cutflows_ee;
 
-    Histo1DPtr _NH_mT2;
-    Histo1DPtr _NH_mll;
-    Histo1DPtr _NH_ETmiss;
-    Histo1DPtr _NH_cThetalp;
-    Histo1DPtr _NH_cThetalm;
-
-    Histo1DPtr _hist_mT2;
-    Histo1DPtr _hist_mll;
-    Histo1DPtr _hist_ETmiss;
-    Histo1DPtr _hist_cThetalp;
-    Histo1DPtr _hist_cThetalm;
-
     // CounterPtr _srcounts[4];
-  };
 
-=======
 
     Histo1DPtr _hist_Elm;
     Histo1DPtr _hist_Elp;
@@ -841,7 +543,6 @@ namespace Rivet
     Histo1DPtr _hist_mRCdM;
 
     ///@}
-  private:
     Cutflows _Cutflows;
     Cutflows _CF_HighDM;
     Cutflows _CF_MidDM;
@@ -852,6 +553,5 @@ namespace Rivet
     Histo1DPtr _SR_lowDELTAM;
   };
 
->>>>>>> 6bcb1bedfa0c084ee36841735834a3ca79f78791
   RIVET_DECLARE_PLUGIN(CEPC_SMUON);
 }
